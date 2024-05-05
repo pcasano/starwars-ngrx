d39e289761d68eca9b2fc2dc73fe1983a13fb399
pcasano <pablo.casanovas@pm.me>
2024-05-05 13:59:08 +0200

    added error in template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { PlanetService } from './store/services/planet.service';
 import { PlanetStore } from './store/planet.store';
 import { Observable } from 'rxjs';
 import { Planet } from './store/models/planets.model';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -14,11 +15,16 @@ export class AppComponent implements OnInit {
 
   planets: Planet[] | undefined | null = [];
   planetsInProgress = true;
+  planetError: HttpErrorResponse | undefined | null
 
   constructor(private readonly planetStore: PlanetStore) {}
 
   ngOnInit(): void {
     this.planetStore.planets$.subscribe(planets => this.planets = planets);
     this.planetStore.planetInProgress$.subscribe(planetsInProgress => this.planetsInProgress = planetsInProgress);
+  }
+
+  loadError() {
+    this.planetStore.planetError$.subscribe(error => this.planetError = error);
   }
 }
